a25cbd7d3c10c20321eb13d0cd6ad6273b4f54a6
ronesta <gvozzzd123@gmail.com>
2024-11-17 17:16:56 +0300

    Initial commit

---
+++ iTunesRealm/iTunesRealm/SearchHistoryViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchHistoryViewController.swift
+//  iTunesRealm
+//
+//  Created by Ибрагим Габибли on 17.11.2024.
+//
+
+import Foundation
+import UIKit
+
+final class SearchHistoryViewController: UIViewController {
+
+}
---
+++ iTunesRealm/iTunesRealm/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  iTunesRealm
+//
+//  Created by Ибрагим Габибли on 17.11.2024.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+}
+
